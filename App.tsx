dfc31c756a363ca8060251cfc9d50ad242050b96
vkstheone <vks7pp2gmail.com>
2024-09-20 15:29:52 -0300

    testando alteraçoes

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react"
 
 function App() {
   
+  const [ name, setName ] = useState("")
+  const [ email, setEmail ] = useState("") 
+  
  
+
+  const handleName = (e: React.ChangeEvent<HTMLInputElement>) =>{
+    const value = e.target.value;  
+    console.log(value)
+    setName(value)
+  }
+  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) =>{
+    const value = e.target.value;  
+    console.log(value)
+    setEmail(value)
+  }
 
   return (
       <div className="tela">
@@ -20,14 +35,17 @@ function App() {
           </div>
         </div>
         <div className="form">
+              <div className="form-text">
               <h3>Crie sua conta </h3>
               <p>Preencha  o formulario</p>
+              </div>
+              <input type="text" placeholder="Nome" value={name} onChange={handleName}/>
 
-              <input type="text" placeholder="Nome" />
-
-              <input type="email" placeholder="Email"/>
+              <input type="email" placeholder="Email" value={email} onChange={handleEmail}/>
 
               <input type="password" placeholder="Senha"/>
+
+              <button>CADASTRAR</button>
         </div>
       </div>
   )
